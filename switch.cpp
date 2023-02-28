ffd3debe4cf1380e3d0bb6489c860460acfab8cc
Henok100 <henokgashaw15@gmail.com>
2023-02-28 16:36:56 +0300

    26

---
+++ switch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+    int age = 21;
+
+    switch (age)
+    {
+    case 21:
+        cout << "Best\n ";
+        break;
+    
+    default:
+        break;
+    }
+
+}
